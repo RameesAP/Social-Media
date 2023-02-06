d5732dc7e6075541a8d4aea6429ce1f9d16611f9
RameesAP <rameeswork@gmail.com>
2023-02-06 20:55:01 +0530

    first commit

---
+++ client/src/components/Data/TrendData.js
@@ -0,0 +1,22 @@
+export const TrendData=[
+    {
+        name:"Minions",
+        shares:97,
+    },
+    {
+        name:"Avangers",
+        shares:80.5,
+    },
+    {
+        name:"Ramees",
+        shares:"75.5",
+    },
+    {
+        name:"Reactjs",
+        shares:72
+    },
+    {
+        name:"elon musk",
+        shares:70
+    }
+]
