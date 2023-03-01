50985b2c48b3dff1f67475b71680c536b8998ad1
RameesAP <rameeswork@gmail.com>
2023-03-01 23:11:12 +0530

    Chat API

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+
+
+import './Chat.css'
+
+import LogoSearch from '../../components/LogoSearch/LogoSearch'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from "react-redux"
 
 const Chat = () => {
-  return (
-    <div>Chat</div>
-  )
+
+
+    const { user } = useSelector((state) => state.authReducer.authData)
+
+    const [chats, setChats] = useState([])
+
+    useEffect(() => {
+        const getChat = async () => {
+            try {
+                const { data } = await userChats(user._id)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, [])
+    return (
+        <div className='Chat'>
+            {/* Left Side */}
+            <div className='Left-side-chat'>
+                <LogoSearch />
+
+                <div className="Chat-container">
+                    <h2>Chats</h2>
+                    <div className='Chat-list'>Conversations</div>
+                </div>
+
+
+            </div>
+
+            {/* Right Side */}
+            <div className='Left-side-chat'>
+                <h2>Right Side</h2>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Chat
