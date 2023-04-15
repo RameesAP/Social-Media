3a08759f1bb645b9d3c094da7b8bd1f09b81580c
RameesAP <rameeswork@gmail.com>
2023-04-15 15:23:09 +0530

    main url

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: "https://frabjous-gnome-9a864e.netlify.app" })
+const main= process.env.MainURL
+const API = axios.create({ baseURL: main })
 
 export const logIn = (formData) => API.post('/auth/login', formData)
 export const signUp = (formData) => API.post('/auth/register', formData)
