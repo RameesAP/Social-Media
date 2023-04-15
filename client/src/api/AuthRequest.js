dde86d08452e41335dd3496662d1103f218294ea
RameesAP <rameeswork@gmail.com>
2023-04-15 18:33:55 +0530

    set

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
-const main= process.env.MainURL
-const API = axios.create({ baseURL: main })
+
+const API = axios.create({ baseURL: "http://localhost:5000" })
+
+// const API = axios.create({ baseURL: "http://localhost:5000" })
 
 export const logIn = (formData) => API.post('/auth/login', formData)
 export const signUp = (formData) => API.post('/auth/register', formData)
--- client/src/api/ChatRequest.js
+++ client/src/api/ChatRequest.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' })
-
+const API = axios.create({ baseURL: 'https://mern-socialmedia-main-server.onrender.com' })
+// const API = axios.create({ baseURL: 'http://localhost:5000' })
 
 export const userChats = (id) => API.get(`/chat/${id}`)
